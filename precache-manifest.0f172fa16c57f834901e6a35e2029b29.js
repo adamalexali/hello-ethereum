71e0d799bb674e7442fb787e8ccb095700d3ed9a
Adam <adamalexali@gmail.com>
2021-11-03 23:01:09 -0400

    Updates

---
+++ precache-manifest.0f172fa16c57f834901e6a35e2029b29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5694fa893d704216302b973bdd999a3",
+    "url": "/hello-ethereum/index.html"
+  },
+  {
+    "revision": "2d5a331084d37fda55b5",
+    "url": "/hello-ethereum/static/css/main.42e3d32a.chunk.css"
+  },
+  {
+    "revision": "7e6a420e505bdf0433fc",
+    "url": "/hello-ethereum/static/js/2.745ab055.chunk.js"
+  },
+  {
+    "revision": "2d5a331084d37fda55b5",
+    "url": "/hello-ethereum/static/js/main.8008921f.chunk.js"
+  },
+  {
+    "revision": "deb0180e6f34fac1167b",
+    "url": "/hello-ethereum/static/js/runtime-main.bcce053b.js"
+  }
+]);
